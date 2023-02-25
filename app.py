f86d572deb7770502b5e685ecadcf4edcb89f948
unknown <aadeshkabra@.com>
2023-02-25 15:02:48 +0530

    Final Commit

--- app.py
+++ app.py
@@ -1,27 +1,9 @@
-import librosa
+
 from flask import Flask, render_template, request
-from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_dl import YoutubeDL
-from pytube import YouTube
-import os
-import speech_recognition as sr
-import ffmpeg
-import wave, math, contextlib
-from moviepy.editor import AudioFileClip
-import io
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
-from google.cloud import speech_v1p1beta1 as speech
-import pyaudio
-import speechmatics
+
 import requests
 from youtube_transcript_api import YouTubeTranscriptApi
 
-import numpy as np
-# import deepspeech
-#
-# model_file_path = 'deepspeech-0.9.3-models.pbmm'
-# model = deepspeech.Model(model_file_path)
 
 app = Flask(__name__)
 
@@ -88,20 +70,8 @@ def get_response(link):
         "authorization": "b2954594ba2543268070bf45ba7d7c97",
     }
 
-    # endpoint = "https://api.assemblyai.com/v2/transcript"
-    # json = {"audio_url": "https://bit.ly/3yxKEIY"}
-    # headers = {
-    #     "authorization": "YOUR-API-TOKEN",
-    # }
     response = requests.post(endpoint, json=json, headers=headers)
     print(response.json())
-    # while True:
-    #     response = requests.get(endpoint, json=json, headers=headers)
-    #     if response.json()["status"] == "processing":
-    #         break
-    # response = requests.get(endpoint, json=json, headers=headers)
-    # print(response)
-    # print(response.json()['transcripts'])
 
     endpoint = "https://api.assemblyai.com/v2/transcript/YOUR-TRANSCRIPT-ID-HERE"
     headers = {
@@ -136,21 +106,6 @@ def input():
     text = ' '.join(text_list)
     print(text)
 
-    # Generating Mp4 file from youtube link
-    # yt = YouTube(link)
-    # stream = yt.streams.get_audio_only()
-    # stream.download(filename="video.mp4")
-
-
-    # transcribed_audio_file_name = "audio.wav"
-    # zoom_video_file_name = "video.mp4"
-    #
-    # audioclip = AudioFileClip(zoom_video_file_name)
-    # audioclip.write_audiofile(transcribed_audio_file_name)
-
-    # text = get_response(link)
-    # text = "Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic random paragraph will be about when it appears. These forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph a the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The thi option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertook, the writer is forced to use creativity to incorporate the paragraph into their writing."
-
     return render_template("index.html", text=text)
 
 
@@ -184,28 +139,36 @@ def askQuestions():
     return render_template("display.html", questions=questions_list)
 
 
-# @app.route("/language", methods=['POST'])
-# def language():
-#     lang = request.form.get("language")
-#     url = "https://v1.genr.ai/api/circuit-element/translate-text"
-#     temp_questions = []
-#
-#     global questions
-#     for question in questions:
-#         payload = {
-#             "text": question,
-#             "temperature": 0.3,
-#             "target_language": lang
-#         }
-#         headers = {"Content-Type": "application/json"}
-#
-#         response = requests.request("POST", url, json=payload, headers=headers)
-#
-#         print(response.text)
-#         temp_questions.append(response.text)
-#
-#     print(temp_questions)
-#     return render_template("display.html", questions=temp_questions)
+@app.route("/language", methods=['POST'])
+def language():
+    lang = request.form.get("language")
+    url = "https://v1.genr.ai/api/circuit-element/translate-text"
+    temp_questions = []
+
+    global questions
+    print(questions)
+    question_str = ""
+    for i in questions:
+        question_str += i
+        question_str += "."
+
+    payload = {
+        "text": question_str,
+        "temperature": 0.3,
+        "target_language": lang
+    }
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+
+    ques_dic = response.text
+    # print(response.text)
+    questions_str = ques_dic.split(':')[1]
+    questions_list = questions_str.split('\\n')
+    questions_list[0] = questions_list[0][1:]
+    questions_list[len(questions_list) - 1] = questions_list[len(questions_list) - 1][:-2]
+    # print(temp_questions)
+    return render_template("display.html", questions=questions_list)
 
 
 
